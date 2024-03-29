60b0ec67ebdae899ffa8deb381556ebaebf08008
MikoTheShiba <mikoangeles2001@gmail.com>
2024-03-29 12:56:36 +0800

    minor commits before I go out for holy week for a couple hours

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # web-resume
 A resume I made in React and MUI
+
+I mostly made this for practice soooo
---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import {
+    Typography, 
+    Button, 
+    Drawer 
+} from "@mui/material";
+const Sidebar = () => {
+    return(
+        <Typography>Hi</Typography>
+    )
+}
+
+export default Sidebar;

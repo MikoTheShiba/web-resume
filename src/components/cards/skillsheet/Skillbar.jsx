b0332ab83f25fcd8099aded1d8d301fe9813a71f
MikoTheShiba <mikoangeles2001@gmail.com>
2024-04-14 21:33:05 +0800

    update Skillbar.jsx

--- src/components/cards/skillsheet/Skillbar.jsx
+++ src/components/cards/skillsheet/Skillbar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Box, Card, Typography } from '@mui/material';
 
-const Skillbar = ({skill, progress}) => {
-  return(
-    <Box>
-    
-    </Box>
-    )
+const Skillbar = ({ skill, progress }) => {
+  return (
+    <Card className='CardEntry'>
+      <Grid container>
+        <Grid item xs={4}>
+          <Typography>`${skill}`</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Stack>
+            <Typography variant='h6'>`${skill}</Typography>
+            <LinearProgress variant="determinate" value={progress} />
+          </Stack>
+        </Grid>
+      </Grid>
+    </Card>
+  )
 }
